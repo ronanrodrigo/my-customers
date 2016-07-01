77ce892cb1209f88b21e453a4e80e849a5fcd99b
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-07-01 18:48:32 -0300

    Created core structure

---
+++ MyCustomersCore/Entities/CustomerEntity.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct CustomerEntity: Customer {
+    var name: String?
+    var phone: String?
+    var email: String?
+    var address: String?
+}
---
+++ MyCustomersCore/ValueObjects/Customer.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+protocol Customer {
+    var name: String? { get set }
+    var phone: String? { get set }
+    var email: String? { get set }
+    var address: String? { get set }
+}
