81a3c069a42337f87d676d3e4c1a3b5138f154f2
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-07-01 13:31:33 -0300

    Updated save form view

--- MyCustomers/ViewControllers/SaveCustomerViewController.swift
+++ MyCustomers/ViewControllers/SaveCustomerViewController.swift
@@ -2,15 +2,12 @@ import UIKit
 
 class SaveCustomerViewController: UIViewController {
 
-    @IBOutlet weak var topPosition: NSLayoutConstraint!
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        topPosition.constant = self.topLayoutGuide.length
     }
 
 }
