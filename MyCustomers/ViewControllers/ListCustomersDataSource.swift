14a6e72a02a5ec9c275d776e2492227b25e99e2e
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-07-01 18:36:51 -0300

    Created data source and added to tableview

---
+++ MyCustomers/ViewControllers/ListCustomersDataSource.swift
@@ -0,0 +1,25 @@
+import Foundation
+import UIKit
+
+class ListCustomersDataSource: NSObject, UITableViewDataSource {
+
+    let customers = []
+    let cellIdentifier = "CustomerCell"
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return customers.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: cellIdentifier)
+
+        if let reuseCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) {
+            cell = reuseCell
+        }
+
+        cell.textLabel?.text = customers[indexPath.row]
+
+        return cell
+    }
+
+}
--- MyCustomers/ViewControllers/ListCustomersViewController.swift
+++ MyCustomers/ViewControllers/ListCustomersViewController.swift
@@ -2,8 +2,21 @@ import UIKit
 
 class ListCustomersViewController: UIViewController {
 
+    // MARK: Properties
+
+    var customersDataSource: ListCustomersDataSource?
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDataSource()
+    }
+
+    // MARK: Setups
+
+    private func configureDataSource() {
+        customersDataSource = ListCustomersDataSource()
+        if let customersDataSource = customersDataSource {
+            tableView.dataSource = customersDataSource
+        }
     }
 
 }
