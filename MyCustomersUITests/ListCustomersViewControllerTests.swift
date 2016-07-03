97412bc1043b8eb7892d9475fbc7971c4a643d95
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-07-02 21:17:42 -0300

    Created new UI tests

--- MyCustomersUITests/ListCustomersViewControllerTests.swift
+++ MyCustomersUITests/ListCustomersViewControllerTests.swift
@@ -1,15 +1,30 @@
 import XCTest
+import MyCustomersCore
 
 class ListCustomersViewControllerTests: XCTestCase {
+
+    var customersRepository: CustomerRepository!
 
     override func setUp() {
         super.setUp()
         continueAfterFailure = false
+        customersRepository = CustomerRepositoryInMemory()
         XCUIApplication().launch()
     }
 
     func testShouldTouchAtFirstItemOfTheList() {
-        XCUIApplication().tables.staticTexts["Ronan Rodrigo Nunes"].tap()
+        let customerName = customersRepository.all().first!.name
+        XCUIApplication().tables.staticTexts[customerName!].tap()
+    }
+
+    func testShouldTouchAtAddCustomer() {
+        XCUIApplication().navigationBars["Customers"].buttons["Add"].tap()
+    }
+
+    func testShouldTouchAtAddCustomerAndBackToList() {
+        let app = XCUIApplication()
+        app.navigationBars["Customers"].buttons["Add"].tap()
+        app.navigationBars["New customer"].buttons["Customers"].tap()
     }
 
 }
