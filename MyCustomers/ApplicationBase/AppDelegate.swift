f60a129425925ed8ed598d388a3547969e6ee7ff
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-10-04 12:30:26 -0300

    Removed custom colors

--- MyCustomers/ApplicationBase/AppDelegate.swift
+++ MyCustomers/ApplicationBase/AppDelegate.swift
@@ -11,8 +11,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        window?.tintColor = #colorLiteral(red: 0.7660271525, green: 0.1211866513, blue: 0.3483361006, alpha: 1)
         window?.makeKeyAndVisible()
 
         customersAppRouter = CustomersRouterApp(window: window)
